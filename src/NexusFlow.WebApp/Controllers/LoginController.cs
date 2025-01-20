ae0747b7ab843b645080daef550dfd646702bdb5
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-20 23:08:40 +0200

    Update LoginController.cs

--- src/NexusFlow.WebApp/Controllers/LoginController.cs
+++ src/NexusFlow.WebApp/Controllers/LoginController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NexusFlow.WebApp.Models;
 
 namespace NexusFlow.WebApp.Controllers
 {
     public class LoginController : Controller
     {
+        //Auth
         public IActionResult Index()
         {
             return View();
@@ -13,7 +15,18 @@ namespace NexusFlow.WebApp.Controllers
         public ActionResult SubmitLogin(string username, string password)
         {
             TempData["SuccessMessage"] = "Thank you for your message! We'll get back to you shortly.";
-            return RedirectToAction("Contact");
+            return RedirectToAction("Persons");
+        }
+
+        //Persons
+        private static List<PersonViewModel> _persons = new()
+    {
+        new PersonViewModel { Id = 1, IdNumber = "123456789", FirstName = "John", LastName = "Doe", AccountNumber = "AC12345" },
+        new PersonViewModel { Id = 2, IdNumber = "987654321", FirstName = "Jane", LastName = "Smith", AccountNumber = "AC54321" }
+    };
+        public IActionResult Persons()
+        {
+            return View(_persons);
         }
     }
 }
