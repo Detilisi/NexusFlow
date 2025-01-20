578ac94bc911f22d3671aa713826f0331af9f82b
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-20 22:12:26 +0200

    Create PersonViewModel.cs

---
+++ src/NexusFlow.WebApp/Models/PersonViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NexusFlow.WebApp.Models
+{
+    public class PersonViewModel
+    {
+        public required int Id { get; set; }
+        public required string IdNumber { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string AccountNumber { get; set; }
+    }
+}
