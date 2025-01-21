e45a33cb63c5020e024d7657f61121d72af15a73
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-21 20:23:51 +0200

    Add person controller

--- src/NexusFlow.WebApp/Controllers/LoginController.cs
+++ src/NexusFlow.WebApp/Controllers/LoginController.cs
@@ -1,32 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using NexusFlow.WebApp.Models;
 
-namespace NexusFlow.WebApp.Controllers
+namespace NexusFlow.WebApp.Controllers;
+
+public class LoginController : Controller
 {
-    public class LoginController : Controller
+    public IActionResult Index()
     {
-        //Auth
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View();
+    }
 
-        [HttpPost]
-        public ActionResult SubmitLogin(string username, string password)
-        {
-            TempData["SuccessMessage"] = "Thank you for your message! We'll get back to you shortly.";
-            return RedirectToAction("Persons");
-        }
-
-        //Persons
-        private static List<PersonViewModel> _persons = new()
+    [HttpPost]
+    public ActionResult SubmitLogin(string username, string password)
     {
-        new PersonViewModel { Id = 1, IdNumber = "123456789", FirstName = "John", LastName = "Doe", AccountNumber = "AC12345" },
-        new PersonViewModel { Id = 2, IdNumber = "987654321", FirstName = "Jane", LastName = "Smith", AccountNumber = "AC54321" }
-    };
-        public IActionResult Persons()
-        {
-            return View(_persons);
-        }
+        TempData["SuccessMessage"] = "Thank you for your message! We'll get back to you shortly.";
+        return RedirectToAction("Index", "Persons");
     }
 }
---
+++ src/NexusFlow.WebApp/Controllers/PersonsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NexusFlow.WebApp.Models;
+
+namespace NexusFlow.WebApp.Controllers;
+
+[Route("Home/[controller]")]
+public class PersonsController : Controller
+{
+    private static List<PersonViewModel> _persons = new()
+    {
+        new PersonViewModel { Id = 1, IdNumber = "123456789", FirstName = "John", LastName = "Doe", AccountNumber = "AC12345" },
+        new PersonViewModel { Id = 2, IdNumber = "987654321", FirstName = "Jane", LastName = "Smith", AccountNumber = "AC54321" }
+    };
+
+    public IActionResult Index()
+    {
+        return View(_persons);
+    }
+}
