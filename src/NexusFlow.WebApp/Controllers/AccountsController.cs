581fa748664bd69a781017188074321c96b1ab4a
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-21 23:27:32 +0200

    Add account index

---
+++ src/NexusFlow.WebApp/Controllers/AccountsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NexusFlow.WebApp.Controllers
+{
+    [Route("Login/Persons/[controller]")]
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
