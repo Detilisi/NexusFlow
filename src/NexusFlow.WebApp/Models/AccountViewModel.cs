81b2d0af4ff045efb9a2c2fd07e8d56a8d1bf0d4
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-22 23:52:27 +0200

    Update AccountViewModel.cs

--- src/NexusFlow.WebApp/Models/AccountViewModel.cs
+++ src/NexusFlow.WebApp/Models/AccountViewModel.cs
@@ -4,9 +4,15 @@
     {
         public int Code { get; set; } = 0;
         public int PersonCode { get; set; } = 0;
-        public int Status { get; set; } = 0;
+        public AccountStatus Status { get; set; } = AccountStatus.Open;
         public string AccountNumber { get; set; } = string.Empty;
         public decimal OutStandingBalance { get; set; } = 00.00m;
         public List<TransactionViewModel> Transactions { get; set; } = [];
+    }
+
+    public enum AccountStatus
+    {
+        Open = 1,
+        Closed = 0
     }
 }
