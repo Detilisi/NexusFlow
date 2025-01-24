4b1d000a2b8cb4a1d5086f9b2961a3c67a42b939
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-24 21:13:56 +0200

    Add database

---
+++ src/NexusFlow.PublicApi/Data/DataAccess.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace NexusFlow.PublicApi.Data;
+
+public class DataAccess
+{
+    private SqlConnection _connection;
+
+    public DataAccess()
+    {
+        _connection = new SqlConnection("Server=localhost;Database=NexusFlow;Trusted_Connection=True;");
+    }
+}
