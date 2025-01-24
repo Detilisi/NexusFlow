1938c45ae7dc02e60e5c28688a4f5a445d26ce3e
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-24 21:43:41 +0200

    Create PersonsRepository.cs

---
+++ src/NexusFlow.PublicApi/Data/Repositories/PersonsRepository.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using NexusFlow.PublicApi.Models;
+using System.Data;
+
+namespace NexusFlow.PublicApi.Data.Repositories;
+
+public class PersonsRepository
+{
+    private readonly DataAccess _dataAccess;
+
+    public PersonsRepository()
+    {
+        _dataAccess = new DataAccess();
+    }
+
+    public async Task<int> CreatePersonAsync(Person person)
+    {
+        using var connection = _dataAccess.GetDbConnection();
+        var parameters = new DynamicParameters();
+        parameters.Add("@IDNumber", person.IdNumber, DbType.String);
+        parameters.Add("@FirstName", person.Name, DbType.String);
+        parameters.Add("@LastName", person.Surname, DbType.String);
+
+        var result = await connection.ExecuteAsync("CreatePerson", parameters, commandType: CommandType.StoredProcedure);
+        return result;
+    }
+
+    public async Task<int> UpdatePersonAsync(Person person)
+    {
+        using var connection = _dataAccess.GetDbConnection();
+        var parameters = new DynamicParameters();
+        parameters.Add("@PersonCode", person.Code, DbType.Int32);
+        parameters.Add("@IDNumber", person.IdNumber, DbType.String);
+        parameters.Add("@FirstName", person.Name, DbType.String);
+        parameters.Add("@LastName", person.Surname, DbType.String);
+
+        var result = await connection.ExecuteAsync("UpdatePerson", parameters, commandType: CommandType.StoredProcedure);
+        return result;
+    }
+
+    public async Task<int> DeletePersonAsync(int personCode)
+    {
+        using var connection = _dataAccess.GetDbConnection();
+        var parameters = new DynamicParameters();
+        parameters.Add("@PersonCode", personCode, DbType.Int32);
+
+        var result = await connection.ExecuteAsync("DeletePerson", parameters, commandType: CommandType.StoredProcedure);
+        return result;
+    }
+
+    public async Task<Person> GetPersonByIDAsync(string idNumber)
+    {
+        using var connection = _dataAccess.GetDbConnection();
+        var parameters = new DynamicParameters();
+        parameters.Add("@IDNumber", idNumber, DbType.String);
+
+        var result = await connection.QueryFirstOrDefaultAsync<Person>("GetPersonByID", parameters, commandType: CommandType.StoredProcedure);
+        return result;
+    }
+
+    public async Task<IEnumerable<Person>> GetAllPersonsAsync()
+    {
+        using var connection = _dataAccess.GetDbConnection();
+        var result = await connection.QueryAsync<Person>("GetAllPersons", commandType: CommandType.StoredProcedure);
+        return result;
+    }
+}
