a70f5de9af72efb35bcfc6433e53767d754687e0
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-25 17:47:42 +0200

    Update DeleteAccount.sql

--- src/NexusFlow.Database/dbo/Stored Procedures/DeleteAccount.sql
+++ src/NexusFlow.Database/dbo/Stored Procedures/DeleteAccount.sql
@@ -1,30 +1,30 @@
 ﻿CREATE PROCEDURE DeleteAccount
-    @account_number VARCHAR(50)
+    @Code INT
 AS
 BEGIN
     -- Check if the account exists
-    IF NOT EXISTS (SELECT 1 FROM Accounts WHERE account_number = @account_number)
+    IF NOT EXISTS (SELECT 1 FROM Accounts WHERE code = @Code)
     BEGIN
         RAISERROR('Account not found.', 16, 1);
         RETURN;
     END
 
     -- Check if the account has a zero balance
-    IF (SELECT outstanding_balance FROM Accounts WHERE account_number = @account_number) <> 0
+    IF (SELECT outstanding_balance FROM Accounts WHERE code = @Code) <> 0
     BEGIN
         RAISERROR('Cannot delete account with a non-zero balance.', 16, 1);
         RETURN;
     END
 
     -- Check if the account has any transactions
-    IF EXISTS (SELECT 1 FROM Transactions WHERE account_code = (SELECT code FROM Accounts WHERE account_number = @account_number))
+    IF EXISTS (SELECT 1 FROM Transactions WHERE account_code = (SELECT code FROM Accounts WHERE code = @Code))
     BEGIN
         RAISERROR('Cannot delete account with transactions.', 16, 1);
         RETURN;
     END
 
     -- Delete the account
-    DELETE FROM Accounts WHERE account_number = @account_number;
+    DELETE FROM Accounts WHERE code = @Code;
 
     -- Return success message
     SELECT 'Account deleted successfully.' AS Message;
