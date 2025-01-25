a4723b3f4c9110139cd01875ad644a6120203286
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-25 21:28:47 +0200

    Update GetAccounts.sql

--- src/NexusFlow.Database/dbo/Stored Procedures/GetAccounts.sql
+++ src/NexusFlow.Database/dbo/Stored Procedures/GetAccounts.sql
@@ -5,40 +5,10 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    -- Scenario 1: Both @PersonCode and @Code = -1, return all records
-    IF @PersonCode = -1 AND @Code = -1
-    BEGIN
-        SELECT a.*
-        FROM Accounts a;
-        RETURN;
-    END;
-
-    -- Scenario 2: @Code = -1 but @PersonCode != -1, return all accounts for the person
-    IF @Code = -1 AND @PersonCode != -1
-    BEGIN
-        SELECT a.*
-        FROM Accounts a
-        WHERE a.person_code = @PersonCode;
-        RETURN;
-    END;
-
-    -- Scenario 3: @PersonCode = -1 but @Code != -1, return the account with code = @Code
-    IF @PersonCode = -1 AND @Code != -1
-    BEGIN
-        SELECT a.*
-        FROM Accounts a
-        WHERE a.code = @Code;
-        RETURN;
-    END;
-
-    -- Scenario 4: Both @PersonCode and @Code != -1, return the matching record
-    IF @PersonCode != -1 AND @Code != -1
-    BEGIN
-        SELECT a.*
-        FROM Accounts a
-        WHERE a.person_code = @PersonCode
-          AND a.code = @Code;
-        RETURN;
-    END;
+    SELECT a.*
+    FROM Accounts a
+    WHERE 
+        (@PersonCode = -1 OR a.person_code = @PersonCode)
+        AND (@Code = -1 OR a.code = @Code);
 END;
 GO
