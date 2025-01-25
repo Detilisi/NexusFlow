1fc964298722684ba1c84f51104c00bc8f31dcea
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2025-01-25 17:47:40 +0200

    Update CreateAccount.sql

--- src/NexusFlow.Database/dbo/Stored Procedures/CreateAccount.sql
+++ src/NexusFlow.Database/dbo/Stored Procedures/CreateAccount.sql
@@ -1,17 +1,17 @@
 ﻿CREATE PROCEDURE CreateAccount
-    @person_code INT,
-    @account_number VARCHAR(50)
+    @PersonCode INT,
+    @AccountNumber VARCHAR(50)
 AS
 BEGIN
     -- Check if the person exists
-    IF NOT EXISTS (SELECT 1 FROM Persons WHERE code = @person_code)
+    IF NOT EXISTS (SELECT 1 FROM Persons WHERE code = @PersonCode)
     BEGIN
         RAISERROR('Person not found.', 16, 1);
         RETURN;
     END
 
     -- Check if the account number already exists
-    IF EXISTS (SELECT 1 FROM Accounts WHERE account_number = @account_number)
+    IF EXISTS (SELECT 1 FROM Accounts WHERE account_number = @AccountNumber)
     BEGIN
         RAISERROR('Account number already exists.', 16, 1);
         RETURN;
@@ -19,8 +19,8 @@ BEGIN
 
     -- Insert the new account (default status is 'Open')
     INSERT INTO Accounts (person_code, account_number, outstanding_balance, status_code)
-    VALUES (@person_code, @account_number, 0, 1); -- Default outstanding_balance is 0
+    VALUES (@PersonCode, @AccountNumber, 0, 1); -- Default outstanding_balance is 0
 
     -- Return the newly created account
-    SELECT * FROM Accounts WHERE account_number = @account_number;
+    SELECT * FROM Accounts WHERE account_number = @AccountNumber;
 END
